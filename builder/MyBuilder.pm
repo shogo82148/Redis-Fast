234b75903a136c234367de47f1909ffcffcea055
Ichinose Shogo <shogo82148@gmail.com>
2013-09-27 08:19:05 +0900

    remove unnecessary dependence

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -3,7 +3,6 @@ use strict;
 use warnings FATAL => 'all';
 use 5.008005;
 use base 'Module::Build::XSUtil';
-use EV::MakeMaker '$installsitearch';
 use Config;
 
 sub new {
@@ -13,7 +12,7 @@ sub new {
         generate_ppport_h    => 'src/ppport.h',
         c_source             => 'src',
         xs_files             => { './src/Redis__Fast.xs' => './lib/Redis/Fast.xs', },
-        include_dirs         => ['src', 'deps/hiredis', "${installsitearch}/EV", $installsitearch],
+        include_dirs         => ['src', 'deps/hiredis'],
         extra_linker_flags => ["deps/hiredis/libhiredis$Config{lib_ext}"],
     );
     $self->do_system($Config{make}, '-C', 'deps/hiredis', 'static');
