b8667a84c79cb63878de1417910194238134e70b
Ichinose Shogo <shogo82148@gmail.com>
2013-10-13 22:30:38 +0900

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0d93c97..885d335 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Redis
    
     {{$NEXT}}
    +
    +0.02 2013-10-13T13:27:37Z
         * Remove needless module
         * Use gmake for building library on BSD platform except KFreeBSD
         * Improve reconnection

--- lib/Redis/Fast.pm
+++ lib/Redis/Fast.pm
@@ -3,7 +3,7 @@ package Redis::Fast;
 # ABSTRACT: Perl binding for Redis database
 BEGIN {
     use XSLoader;
-    our $VERSION = '0.01';
+    our $VERSION = '0.02';
     XSLoader::load __PACKAGE__, $VERSION;
 }
 
