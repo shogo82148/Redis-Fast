498f26b39154fddd97b6efcfe278b7597a4fe9c9
Ichinose Shogo <shogo82148@gmail.com>
2013-12-18 22:03:50 +0900

    fix leak in connect callack

--- lib/Redis/Fast.pm
+++ lib/Redis/Fast.pm
@@ -13,7 +13,25 @@ use Data::Dumper;
 use Carp qw/confess/;
 use Encode;
 use Try::Tiny;
+use Scalar::Util qw(weaken);
 
+sub _new_on_connect_cb {
+    my ($self, $on_conn, $password, $name) = @_;
+    weaken $self;
+    return sub {
+        try {
+            $self->auth($password) if defined $password;
+        } catch {
+            confess("Redis server refused password");
+        };
+        try {
+            my $n = $name;
+            $n = $n->($self) if ref($n) eq 'CODE';
+            $self->client_setname($n) if defined $n;
+        };
+        $on_conn->($self) if $on_conn;
+    };
+}
 
 sub new {
   my $class = shift;
@@ -48,21 +66,7 @@ sub new {
   my $on_conn = $args{on_connect};
   my $password = $args{password};
   my $name = $args{name};
-  $self->__set_on_connect(
-      sub {
-          try {
-              $self->auth($password) if defined $password;
-          } catch {
-              confess("Redis server refused password");
-          };
-          try {
-              my $n = $name;
-              $n = $n->($self) if ref($n) eq 'CODE';
-              $self->client_setname($n) if defined $n;
-          };
-          $on_conn->($self) if $on_conn;
-      }
-  );
+  $self->__set_on_connect($self->_new_on_connect_cb($on_conn, $password, $name));
   $self->__set_data({
       subscribers => {},
   });
@@ -163,6 +167,7 @@ sub __subscription_cmd {
   my $unsub   = shift;
   my $command = shift;
   my $cb      = pop;
+  weaken $self;
 
   confess("Missing required callback in call to $command(), ")
     unless ref($cb) eq 'CODE';
